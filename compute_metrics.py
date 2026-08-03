1c8e822feed9e58f80c569fe7525e84173018ee8
mariusbock <marius.bock.work@icloud.com>
2026-08-03 09:13:08 +0200

    updated metrics script to match downstream experiments; updated confusion matrices

--- compute_metrics.py
+++ compute_metrics.py
@@ -7,16 +7,17 @@ import json
 from matplotlib import pyplot as plt
 import numpy as np
 import pandas as pd
-from sklearn.metrics import confusion_matrix, f1_score, precision_score, recall_score
+from sklearn.metrics import confusion_matrix
 import seaborn as sns
 from src.utils.map_metric import ANETdetection
 from src.utils.data_utils import convert_samples_to_segments
+from src.utils.logging_utils import classification_scores
 import warnings
 warnings.simplefilter(action='ignore', category=FutureWarning)
 
 # postprocessing parameters
 types = ['main', 'ablation/attention', 'ablation/contextlength', 'ablation/twolayered']
-datasets = ['opportunity', 'wetlab', 'sbhar', 'rwhar', 'wear', 'hangtime'] 
+datasets = ['hangtime', 'opportunity', 'wetlab', 'sbhar', 'rwhar', 'wear'] 
 seeds = [1, 2, 3]
 
 for type in types:
@@ -34,9 +35,9 @@ for type in types:
             for path in path_to_preds:
                 if not os.path.exists(path):
                     print("Path does not exist: {}".format(path))
-                    continue                    
+                    continue
                 else:
-                    print("Processing path: {}".format(path))
+                    print("\nProcessing path: {}".format(path))
                 if dataset == 'opportunity':
                     num_classes = 18
                     sampling_rate = 30
@@ -140,7 +141,7 @@ for type in types:
                     num_classes = 19
                     sampling_rate = 50
                     input_dim = 12
-                    classes = ['null', 'jogging', 'jogging (rotating arms)', 'jogging (skipping)', 'jogging (sidesteps)', 'jogging (butt-kicks)', 'stretching (triceps)', 'stretching (lunging)', 'stretching (shoulders)', 'stretching (hamstrings)', 'stretching (lumbar rotation)', 'push-ups', 'push-ups (complex)', 'sit-ups', 'sit-ups (complex)', 'burpees', 'lunges', 'lunges (complex)', 'bench-dips']        
+                    classes = ['null', 'jogging', 'jogging (rotating arms)', 'jogging (skipping)', 'jogging (sidesteps)', 'jogging (butt-kicks)', 'stretching (triceps)', 'stretching (lunging)', 'stretching (shoulders)', 'stretching (hamstrings)', 'stretching (lumbar rotation)', 'push-ups', 'push-ups (complex)', 'sit-ups', 'sit-ups (complex)', 'burpees', 'lunges', 'lunges (complex)', 'bench-dips']
                     json_files = [
                     'data/wear/annotations/loso_sbj_0.json',
                     'data/wear/annotations/loso_sbj_1.json',
@@ -166,7 +167,7 @@ for type in types:
                     'data/wear/annotations/loso_sbj_21.json',
                     ]
                 elif dataset == 'hangtime':
-                    num_classes = 7
+                    num_classes = 6
                     sampling_rate = 50
                     input_dim = 3
                     classes = ['null', 'dribbling', 'shot', 'pass', 'rebound', 'layup']
@@ -198,9 +199,10 @@ for type in types:
                     ]
 
                 all_mAP = np.zeros((5, len(json_files)))
-                all_recall = np.zeros((num_classes, len(json_files)))
-                all_prec = np.zeros((num_classes, len(json_files)))
-                all_f1 = np.zeros((num_classes, len(json_files)))
+                all_prec_seeds = np.zeros((num_classes, len(seeds)))
+                all_recall_seeds = np.zeros((num_classes, len(seeds)))
+                all_f1_seeds = np.zeros((num_classes, len(seeds)))
+
                 for s_pos, seed in enumerate(seeds):
                     all_preds = np.array([])
                     all_gt = np.array([])
@@ -214,76 +216,74 @@ for type in types:
                                 labels = ['null'] + list(file['label_dict'])
                             label_dict = dict(zip(labels, list(range(len(labels)))))
                             val_sbjs = [x for x in anno_file if anno_file[x]['subset'] == 'Validation']
-                        
+
                         v_data = np.empty((0, input_dim + 2))
-                            
+
                         for sbj in val_sbjs:
                             data = pd.read_csv(os.path.join('data/{}/raw/inertial'.format(dataset), sbj + '.csv'), index_col=False, low_memory=False).replace({"label": label_dict}).fillna(0).to_numpy()
                             v_data = np.append(v_data, data, axis=0)
-                            
+
                         v_preds = np.array([])
                         if 'loso' in j:
                             v_orig_preds = np.load(os.path.join(path, str(seed), 'unprocessed_results/v_preds_loso_sbj_{}.npy'.format(int(i))))
                         else:
                             v_orig_preds = np.load(os.path.join(path, str(seed), 'unprocessed_results/v_preds_split_{}.npy'.format(int(i) + 1)))
-                            
+
                         for sbj in val_sbjs:
                             sbj_pred = v_orig_preds[v_data[:, 0] == int(sbj.split("_")[-1])]
-                            v_preds = np.append(v_preds, sbj_pred)
+                            v_preds = np.append(v_preds, sbj_pred) 
 
                         seg_data = convert_samples_to_segments(v_data[:, 0], v_preds, sampling_rate)
-                        det_eval = ANETdetection(j, 'validation', tiou_thresholds = [0.3, 0.4, 0.5, 0.6, 0.7])
+                        det_eval = ANETdetection(j, 'validation', tiou_thresholds=[0.3, 0.4, 0.5, 0.6, 0.7])
                         v_mAP, _ = det_eval.evaluate(seg_data)
-                        v_prec = precision_score(v_data[:, -1].astype(int), v_preds, average=None, labels=range(0, num_classes), zero_division=0)
-                        v_rec = recall_score(v_data[:, -1].astype(int), v_preds, average=None, labels=range(0, num_classes), zero_division=0)
-                        v_f1 = f1_score(v_data[:, -1].astype(int), v_preds, average=None, labels=range(0, num_classes), zero_division=0)
-                                                        
-                        all_prec[:, i] += v_prec
-                        all_recall[:, i] += v_rec
-                        all_f1[:, i] += v_f1
+
                         all_mAP[:, i] += v_mAP
                         all_preds = np.append(all_preds, v_preds)
                         all_gt = np.append(all_gt, v_data[:, -1])
 
+                    _, v_prec, v_rec, v_f1 = classification_scores(all_gt.astype(int), all_preds.astype(int), num_classes)
+                    all_prec_seeds[:, s_pos] = v_prec
+                    all_recall_seeds[:, s_pos] = v_rec
+                    all_f1_seeds[:, s_pos] = v_f1
+
                     if seed == 1:
-                        comb_conf = confusion_matrix(all_gt.astype(int), all_preds.astype(int), normalize='true')
+                        comb_conf = confusion_matrix(all_gt.astype(int), all_preds.astype(int), normalize='true', labels=range(num_classes))
                         comb_conf = np.around(comb_conf, 2)
                         comb_conf[comb_conf == 0] = np.nan
 
                         _, ax = plt.subplots(figsize=(15, 15), layout="constrained")
                         sns.heatmap(comb_conf, annot=True, fmt='g', ax=ax, cmap=plt.cm.Purples, cbar=False, annot_kws={'fontsize': 16,}, linecolor='black', vmin=0, vmax=1)
-                        #ax.set_xlabel('Predicted', fontsize=20)
-                        #ax.set_ylabel('True', fontsize=20)
-                        #ax.set_xticklabels(classes, rotation=45, ha='right', fontsize=16)
-                        #ax.set_yticklabels(classes, rotation=0, ha='right', fontsize=16)
-                        ax.set_xticklabels([])
-                        ax.set_yticklabels([])
+                        ax.set_xlabel('Predicted', fontsize=20)
+                        ax.set_ylabel('True', fontsize=20)
+                        ax.set_xticklabels(classes, rotation=45, ha='right', fontsize=16)
+                        ax.set_yticklabels(classes, rotation=0, ha='right', fontsize=16)
                         ax.set_xlim(0, comb_conf.shape[1])
                         ax.set_ylim(comb_conf.shape[0], 0)
                         ax.tick_params(which="both", length=0)
                         pred_name = path.split('/')[-3] + "_" + path.split('/')[-2] + "_" + path.split('/')[-1]
-                        #ax.set_title(pred_name, fontsize=20)
+                        ax.set_title(pred_name, fontsize=20)
                         os.path.exists('confusion_matrices') or os.makedirs('confusion_matrices')
                         _.savefig(os.path.join('confusion_matrices', pred_name + ".svg"), format='svg')
                         plt.close()
-                        
+
                 print("Individual mAP:")
                 print("mAP@0.3: {:.2f} (±{:.2f})".format(np.mean(all_mAP[0, :]) / len(seeds) * 100, np.std(all_mAP[0, :] / len(seeds)) * 100))
                 print("mAP@0.4: {:.2f} (±{:.2f})".format(np.mean(all_mAP[1, :]) / len(seeds) * 100, np.std(all_mAP[1, :] / len(seeds)) * 100))
                 print("mAP@0.5: {:.2f} (±{:.2f})".format(np.mean(all_mAP[2, :]) / len(seeds) * 100, np.std(all_mAP[2, :] / len(seeds)) * 100))
                 print("mAP@0.6: {:.2f} (±{:.2f})".format(np.mean(all_mAP[3, :]) / len(seeds) * 100, np.std(all_mAP[3, :] / len(seeds)) * 100))
                 print("mAP@0.7: {:.2f} (±{:.2f})".format(np.mean(all_mAP[4, :]) / len(seeds) * 100, np.std(all_mAP[4, :] / len(seeds)) * 100))
-                
+
                 print("Average mAP:")
                 print("{:.2f} (±{:.2f})".format(np.mean(np.mean(all_mAP / len(seeds), axis=0)) * 100, np.std(np.mean(all_mAP / len(seeds), axis=0)) * 100))
 
                 print("Average precision:")
-                print("{:.2f}% (±{:.2f})".format(np.mean(np.mean(all_prec / len(seeds), axis=0)) * 100, np.std(np.mean(all_prec / len(seeds), axis=0)) * 100))
-                
-                print("Average recall:")
-                print("{:.2f}% (±{:.2f})".format(np.mean(np.mean(all_recall / len(seeds), axis=0)) * 100, np.std(np.mean(all_recall  / len(seeds), axis=0)) * 100))
-                
-                print("Average f1:")
-                print("{:.2f}% (±{:.2f})".format(np.mean(np.mean(all_f1 / len(seeds), axis=0)) * 100, np.std(np.mean(all_f1 / len(seeds), axis=0) ) * 100))
+                per_seed_prec = np.mean(all_prec_seeds, axis=0)
+                print("{:.2f}% (±{:.2f})".format(np.mean(per_seed_prec) * 100, np.std(per_seed_prec) * 100))
 
-    
+                print("Average recall:")
+                per_seed_rec = np.mean(all_recall_seeds, axis=0)
+                print("{:.2f}% (±{:.2f})".format(np.mean(per_seed_rec) * 100, np.std(per_seed_rec) * 100))
+
+                print("Average f1:")
+                per_seed_f1 = np.mean(all_f1_seeds, axis=0)
+                print("{:.2f}% (±{:.2f})".format(np.mean(per_seed_f1) * 100, np.std(per_seed_f1) * 100))
